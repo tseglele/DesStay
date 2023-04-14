f7f28a4fe17c5c4a593df0fab40d180346e4cb3d
tseglele <127389547+tseglele@users.noreply.github.com>
2023-04-14 14:00:48 +0200

    Add files via upload

---
+++ JTK_project(1)/JTK project/project.js
@@ -0,0 +1,78 @@
+function dropdown_open(){
+if(n % 2 ==  0){
+  
+  document.querySelector('.cont_back_menu').className = "cont_back_menu active";
+  document.getElementById('svg_icon').style.top = "0px";
+  
+  n++;
+}else{
+  document.querySelector('.cont_back_menu').className = "cont_back_menu  inactive";
+  n++;
+setTimeout(function(){
+ 
+  document.querySelector('.cont_back_menu').className = "cont_back_menu  div_hidde";
+
+  document.getElementById('svg_icon').style.top = "-13px";    
+},1200);
+
+}
+
+}
+
+const KEY = "91f8b0e1eec5642338861797e5607b54";
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=1`;
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&query=`;
+
+const main = document.getElementById("containerr");
+const filmsearch = document.getElementById("filmsearch");
+const search = document.getElementById("search");
+const btn = document.getElementById("btnSearch1");
+
+const getClassByRate = (vote) => {
+  if (vote >= 7.5) return "green";
+  else if (vote >= 7) return "orange";
+  else return "red";
+};
+
+const showMovies = (movies) => {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieElement = document.createElement("div");
+    movieElement.classList.add("movie");
+    movieElement.innerHTML = `
+    <img
+      src="${IMG_PATH + poster_path}"
+      alt="${title}"
+    />
+    <div class="movie-info">
+      <h3>${title}</h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+    </div>
+    <div class="overview">
+      <h3>Overview</h3>
+      ${overview}
+    </div>
+  `;
+    main.appendChild(movieElement);
+  });
+};
+
+const getMovies = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  showMovies(data.results);
+};
+
+getMovies(API_URL);
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = "";
+  } else history.go(0);
+  btn.innerText="return";
+});
